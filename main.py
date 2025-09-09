772a5982474c980d8fb390d3c90f45196669dcaa
Ya.Zhang <ya.zhang@quantil.com>
2025-09-09 17:14:57 +0800

    py2 test1

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm2')
+    print_hi('PyCharm2,zhangya')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
