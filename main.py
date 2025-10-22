d15868804730a844155f9eb0bd69d7c5032ba594
iceberg <iceberg@wangsu.com>
2025-10-22 13:55:26 +0800

    py2 test2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import sys
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -13,4 +13,251 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm2,zhangya')
     print_hi('hello,world')
+
+print('\n\nPython 路径为：', sys.path, '\n')
+
+
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+tt = (1, 2, 3, 4, 5, 6)
+it = iter(tt)
+while True:
+    try:
+        print(next(it))
+    except StopIteration:
+        break
+
+print( "="*10 )
+class MyIterator:
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.data):
+            value = self.data[self.index]
+            self.index += 1
+            return value
+        else:
+            raise StopIteration
+
+# 使用
+my_iter = MyIterator((1,2,3,'a'))
+for num in my_iter:
+    print(num)
+
+def my_generator(data):
+    for i in data:
+        yield i * 2
+
+gen = my_generator(('a','d','a','f'))
+for doubled in gen:
+    print(doubled)
+
+def fabonacci(n):
+    a, b, counter = 0,1,0
+    while True:
+        if (counter > n):
+            return
+        yield a
+        a, b = b, a + b
+        counter += 1
+f = fabonacci(10)
+
+while True:
+    try:
+        print(next(f),end=" ")
+    except StopIteration:
+        break
+
+print()
+with open(r"E:\pythonProject\learnPY2\test_file.txt",'r') as file:
+    content = file.read()
+    print(content)
+def pizza_slicer(pizza_size):
+    """无限切披萨的生成器"""
+    slices = pizza_size // 8  # 计算能切多少片
+    print(slices)
+    for slice_num in range(slices):
+        yield f"第{slice_num + 1}片披萨"
+
+    yield "披萨切完了！"
+
+
+# 使用披萨切片机
+pizza = pizza_slicer(16)  # 一个16寸的披萨
+
+for slice in pizza:
+    print(f"享用: {slice}")
+
+
+class Timer:
+    def __enter__(self):
+        import time
+        self.start = time.time()
+        print(f"start: {self.start:.2f}")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        import time
+        self.end = time.time()
+        print(f"end: {self.end:.3f}")
+        print(f"耗时: {self.end - self.start:.3f}秒")
+        return False
+
+
+# 使用示例
+with Timer() as t:
+    # 执行一些耗时操作
+    sum(range(1000000))
+
+def printinfo(arg1, *vartuple):
+     print("output:")
+     print(arg1)
+     for var in vartuple:
+         print(var)
+     return
+
+printinfo(10)
+printinfo(10,20,30)
+
+
+def printinfo1(arg1, **vardict):
+    print("output:")
+    print(arg1)
+    print(vardict)
+    return
+
+
+printinfo1(10)
+printinfo1(10, a=20, b=30)
+
+sum = lambda a,b: a+b
+print("sum:",sum(10,20))
+
+def myfunc(n):
+    return lambda a:a*n
+
+mydouble=myfunc(2)
+mytriple=myfunc(3)
+
+print(mydouble(11))
+print(mytriple(11))
+
+
+def sum1(arg1, arg2):
+    # 返回2个参数的和."
+    total = arg1 + arg2
+    print("函数内 : ", total)
+    print(id(total))
+    return total
+
+
+# 调用sum函数
+total = sum1(10, 20)
+print("函数外 : ", total)
+print(id(total))
+
+def draw_rectangle(x, y, width, height, /, color="black", p=5, *, fill=False):
+    print(f"在({x},{y})绘制{width}x{height}的矩形，颜色:{color}, p:{p},填充:{fill}")
+draw_rectangle(10, 20, 100, 50)                    # 正确
+draw_rectangle(10, 20, 100, 50, color="red",p=4)       # 正确
+draw_rectangle(10, 20, 100, 50, fill=True)
+
+def my_decorator(func):
+    def wrapper():
+        print("before original func running")
+        func()
+        print("after original func running")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("hello")
+say_hello()
+
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args,**kwargs)
+        end = time.time()
+        print(f"{func.__name__} run time: {end - start:.2f} seconds")
+        return result
+    return wrapper
+
+@timer
+def slow_func():
+    time.sleep(1)
+    return("done")
+
+result=slow_func()
+print(result)
+
+from collections import deque
+
+q=deque()
+
+q.append('a')
+q.append('b')
+q.append('c')
+
+print(q)
+first_element=q.popleft()
+print(first_element)
+
+vec=[2,4,6]
+vec1=[3*x for x in vec]
+print(vec)
+print(vec1)
+
+vec2=[[x,x**2] for x in vec]
+print(vec2)
+
+vec3=[1,2,3]
+vec4=[x*y for x in vec for y in vec3]
+print(vec4)
+
+matrix=[[1,2,3,4],
+        [5,6,7,8],
+        [9,10,11,12]
+        ]
+print(matrix)
+
+matrix_r=[[row[i] for row in matrix] for i in range(4)]
+print(matrix_r)
+
+matrix_r1=[]
+for i in range(4):
+    new_row=[]
+    for row in matrix:
+        new_row.append(row[i])
+    matrix_r1.append(new_row)
+print(matrix_r1)
+
+import fibo
+
+fibo.fib(10)
--- wordcounter.py
+++ wordcounter.py
@@ -38,6 +38,248 @@ if __name__ == "__main__":
     except FileNotFoundError:
         print(f"File not found")
 
+str='123456789'
+print(str)
+print(str[0:1])
+print(str[0:-2])
+print(str[2:5])
+print(str[3:])
+print(str[1:5:2])
+print("-"*10)
+print(str+'hello',end="")
+print(str*2)
+print("hello\nworld")
+print(r"hello\nworld")
+
+# input("\n\n按下 enter 键后退出。")
+
+import sys; x='fdsadfaf'; sys.stdout.write(x);sys.stdout.write(x+'\n');
+
+from sys import argv,path
+
+print('================Python import mode==========================')
+print ('命令行参数为:')
+for i in argv:
+    print(i)
+print('\n python path is',path)
+
+# 变量定义
+x = 10          # 整数
+y = 3.14         # 浮点数
+name = "Alice"   # 字符串
+is_active = True # 布尔值
+
+# 多变量赋值
+a, b, c = 1, 2, "three"
+
+# 查看数据类型
+print(type(x))        # <class 'int'>
+print(type(y))        # <class 'float'>
+print(type(name))     # <class 'str'>
+print(type(is_active)) # <class 'bool'>
+
+# 不可变示例
+x = 10
+print(f"x的id: {id(x)}")  # 输出: x的id: 140736053260736 (示例值)
+
+x = x + 1  # 看起来是修改，实际上是创建新对象
+print(f"修改后x的id: {id(x)}")  # 输出不同的id值
+
+# 另一个例子
+a = 5
+b = 5
+print(f"a is b: {a is b}")  # 输出: True (Python会重用小整数对象)
+
+t=(1,2,3)
+t=(10,)+t[1:]
+print(t)
+
+list=[1,'123','haha',3.14,100]
+list1=['abc',456]
+print(list)
+print(list+list1)
+list[3]=3.1415
+print(list)
+print(list[0::1])
+print(list[-1::-1])
+
+s='abcdefg'
+print(s[-1::-1])
+
+
+def reverseWords(input):
+    # 通过空格将字符串分隔符，把各个单词分隔为列表
+    inputWords = input.split(" ")
+
+    # 翻转字符串
+    # 假设列表 list = [1,2,3,4],
+    # list[0]=1, list[1]=2 ，而 -1 表示最后一个元素 list[-1]=4 ( 与 list[3]=4 一样)
+    # inputWords[-1::-1] 有三个参数
+    # 第一个参数 -1 表示最后一个元素
+    # 第二个参数为空，表示移动到列表末尾
+    # 第三个参数为步长，-1 表示逆向
+    inputWords = inputWords[-1::-1]
+
+    # 重新组合字符串
+    output = ' '.join(inputWords)
+
+    return output
+
+input = 'I like runoob'
+rw = reverseWords(input)
+print(rw)
+
+ss=set()
+ss={1,2,2,3}
+print(ss)
+if(3 in ss):
+    print("3 in ss")
+else:
+    print("3 not in ss")
+
+a = set('abracadabra')
+b = set('alacazam')
+
+print(a)
+
+print(a - b)
+print(a | b)
+print(a & b)
+print(a ^ b)
+
+dict3= {}
+dict3['one'] = "111"
+dict3[2] = '222'
+print(dict3)
+print(dict3[2])
+
+tinydict = {'name': 'runoob','code':1, 'site': 'www.runoob.com'}
+print(tinydict)
+print(tinydict.keys())
+print(tinydict.values())
+
+dict1={x: x**2 for x in (2, 4, 6, 8)}
+print(dict1)
+dict2 = dict([('Runoob', 1), ('Google', 2), ('Taobao', 3)])
+print(dict2)
+
+a = 20
+b = 20
+
+if (a is b):
+    print("1 - a 和 b 有相同的标识")
+else:
+    print("1 - a 和 b 没有相同的标识")
+
+if (id(a) == id(b)):
+    print("2 - a 和 b 有相同的标识")
+else:
+    print("2 - a 和 b 没有相同的标识")
+
+b = 30
+if (a is b):
+    print("3 - a 和 b 有相同的标识")
+else:
+    print("3 - a 和 b 没有相同的标识")
+
+if (a is not b):
+    print("4 - a 和 b 没有相同的标识")
+else:
+    print("4 - a 和 b 有相同的标识")
+
+import time
+
+for i in range(101): # 添加进度条图形和百分比
+    bar = '[' + '=' * (i // 2) + ' ' * (50 - i // 2) + ']'
+    print(f"\r{bar} {i:3}%", end='', flush=True)
+    #time.sleep(0.05)
+print()
+
+x = True
+country_counter = {}
+
+def addone(country):
+    if country in country_counter:
+        country_counter[country] += 1
+    else:
+        country_counter[country] = 1
+
+addone('China')
+addone('Japan')
+addone('China')
+
+print(len(country_counter))
+
+a=set()
+b={1,2,3,4}
+print(a^b)
+
+thisset = set(("Google", "Runoob", "Taobao"))
+thisset.update("Facebook")
+print(thisset)
+
+thisset.update({"Facebook"})
+print(thisset)
+
+ms = [i for i in range(100) if i % 3 == 0 ]
+print(ms)
+
+listdemo = ['Google','Runoob', 'Taobao']
+nd = {key:len(key) for key in listdemo}
+print(nd)
+
+l2 = {x: x**2 for x in (2, 4, 6)}
+print(l2)
+
+a = {x for x in 'abdsdafsdfwfd' if x not in 'abc'}
+print(a)
+
+print("===="*5)
+t = (x**2 for x in range(10))
+b = tuple(t)
+print(b)
+for i in t:
+    print("*")
+    print(i)
+
+# 创建生成器
+gen = (x**2 for x in range(5))
+print("生成器已创建，但尚未计算任何值")
+# 只有在迭代时才会计算
+for value in gen:
+    print(f"计算得到: {value}")
+
+list=[1,2,3,4]
+it = iter(list)
+
+while True:
+    try:
+        print(next(it))
+    except StopIteration:
+        sys.exit()
+
+
+print('+'*10)
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
 
 
 
